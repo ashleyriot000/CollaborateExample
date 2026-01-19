009e053c46403fd900537d82bac2cc4ae0efd845
DESKTOP-6J3UHLI\EZENIC <rlaehdtjrrlaehdtjr3@naver.com>
2026-01-19 12:07:38 +0900

    충돌 해결

--- Assets/Colloborate/Ashley/Colloborate.cs
+++ Assets/Colloborate/Ashley/Colloborate.cs
@@ -4,13 +4,7 @@ public class Colloborate : MonoBehaviour
 {
 	private void Start()
 	{
-<<<<<<< HEAD
         Debug.Log("트롤짓이 시작되었습니다.");
-
-    }
-=======
 		gameObject.SetActive(false);
-		
 	}
->>>>>>> fe737f976f746f3fdc1b023561febe6a8d896a9a
 }
